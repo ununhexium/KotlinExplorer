2c0534a35a87269bdf2b42df82eeaff9ee7161f8
Christophe '116' Loiseau <116@lab0.net>
2021-02-12 14:35:09 +0100

    Big format

--- buildSrc/src/main/java/dependencies/Application.kt
+++ buildSrc/src/main/java/dependencies/Application.kt
@@ -1,7 +1,7 @@
 package dependencies
 
 object Application {
-    val id = "net.lab0.jetpackcomposeexplorer"
-    val versionCode = 1
-    val versionName = "1.0"
+  val id = "net.lab0.jetpackcomposeexplorer"
+  val versionCode = 1
+  val versionName = "1.0"
 }
