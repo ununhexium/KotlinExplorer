e5647b35969fdcbc6fd4ef8618205bc76faf0123
Christophe '116' Loiseau <116@lab0.net>
2021-02-28 10:51:01 +0100

    Reformat gradle kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,182 +1,181 @@
 import dependencies.AnnotationProcessing
-import dependencies.Dependencies
 import dependencies.Application
+import dependencies.Dependencies
 import dependencies.TestDependencies
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("com.android.application")
-    id("com.google.gms.google-services")
-    id("dagger.hilt.android.plugin")
-    id("kotlin-android")
-    id("kotlin-android-extensions")
-    id("kotlin-kapt")
-    id("androidx.navigation.safeargs.kotlin")
+  id("com.android.application")
+  id("com.google.gms.google-services")
+  id("dagger.hilt.android.plugin")
+  id("kotlin-android")
+  id("kotlin-android-extensions")
+  id("kotlin-kapt")
+  id("androidx.navigation.safeargs.kotlin")
 
-    id("org.barfuin.gradle.taskinfo") version "1.0.5"
+  id("org.barfuin.gradle.taskinfo") version "1.0.5"
 }
 
 android {
-    compileSdkVersion(Versions.compileSdk)
+  compileSdkVersion(Versions.compileSdk)
 
-    defaultConfig {
-        applicationId = Application.id
-        minSdkVersion(Versions.minSdk)
-        targetSdkVersion(Versions.targetSdk)
-        versionCode = Application.versionCode
-        versionName = Application.versionName
+  defaultConfig {
+    applicationId = Application.id
+    minSdkVersion(Versions.minSdk)
+    targetSdkVersion(Versions.targetSdk)
+    versionCode = Application.versionCode
+    versionName = Application.versionName
 
-        testInstrumentationRunner(
-            "net.lab0.kotlinexplorer.injection.CustomTestRunner"
+    testInstrumentationRunner(
+        "net.lab0.kotlinexplorer.injection.CustomTestRunner"
+    )
+
+    javaCompileOptions {
+      annotationProcessorOptions {
+        arguments += listOf(
+            "room.schemaLocation" to "$projectDir/schemas"
         )
-
-        javaCompileOptions {
-            annotationProcessorOptions {
-                arguments += listOf(
-                        "room.schemaLocation" to "$projectDir/schemas"
-                )
-            }
-        }
+      }
     }
+  }
 
-    buildFeatures {
-        compose = true
+  buildFeatures {
+    compose = true
+  }
+
+  buildTypes {
+    release {
+      minifyEnabled(false)
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
     }
+  }
 
-    buildTypes {
-        release {
-            minifyEnabled(false)
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
+  compileOptions {
+    sourceCompatibility(Versions.java)
+    targetCompatibility(Versions.java)
+  }
 
-    compileOptions {
-        sourceCompatibility(Versions.java)
-        targetCompatibility(Versions.java)
-    }
+  composeOptions {
+    kotlinCompilerVersion = Versions.kotlin
+    kotlinCompilerExtensionVersion = Versions.compose
+  }
 
-    composeOptions {
-        kotlinCompilerVersion = Versions.kotlin
-        kotlinCompilerExtensionVersion = Versions.compose
-    }
-
-    kotlinOptions {
-        jvmTarget = Versions.java
-        useIR = true
-    }
+  kotlinOptions {
+    jvmTarget = Versions.java
+    useIR = true
+  }
 
 
-    packagingOptions {
-        exclude("META-INF/LICENSE.md")
-        exclude("META-INF/LICENSE-notice.md")
-    }
+  packagingOptions {
+    exclude("META-INF/LICENSE.md")
+    exclude("META-INF/LICENSE-notice.md")
+  }
 
 }
 
-allprojects {
-    tasks {
-        withType<Test> {
-            useJUnitPlatform()
-        }
 
-        withType<KotlinCompile> {
-            kotlinOptions {
-                jvmTarget = "1.8"
-                freeCompilerArgs = freeCompilerArgs + listOf(
-                    "-Xallow-jvm-ir-dependencies",
-                    "-Xskip-prerelease-check",
-                    // to disable @ExperimentalCoroutinesApi warnings
-                    "-Xuse-experimental=kotlin.Experimental"
-                )
-            }
-        }
+tasks {
+  withType<Test> {
+    useJUnitPlatform()
+  }
+
+  withType<KotlinCompile> {
+    kotlinOptions {
+      jvmTarget = "1.8"
+      freeCompilerArgs = freeCompilerArgs + listOf(
+          "-Xallow-jvm-ir-dependencies",
+          "-Xskip-prerelease-check",
+          // to disable @ExperimentalCoroutinesApi warnings
+          "-Xuse-experimental=kotlin.Experimental"
+      )
     }
+  }
 }
 
 
 dependencies {
-    implementation(Dependencies.app_compat)
+  implementation(Dependencies.app_compat)
 
-    implementation(Dependencies.constraintLayout)
+  implementation(Dependencies.constraintLayout)
 
-    implementation(Dependencies.compose_ui)
-    implementation(Dependencies.compose_uiTooling)
-    implementation(Dependencies.compose_foundation)
-    implementation(Dependencies.compose_livedata)
-    implementation(Dependencies.compose_rxJava2)
-    implementation(Dependencies.compose_material)
-    implementation(Dependencies.compose_materialIconsCore)
-    implementation(Dependencies.compose_materialIconsExtended)
+  implementation(Dependencies.compose_ui)
+  implementation(Dependencies.compose_uiTooling)
+  implementation(Dependencies.compose_foundation)
+  implementation(Dependencies.compose_livedata)
+  implementation(Dependencies.compose_rxJava2)
+  implementation(Dependencies.compose_material)
+  implementation(Dependencies.compose_materialIconsCore)
+  implementation(Dependencies.compose_materialIconsExtended)
 
-    implementation(platform(Dependencies.firebase))
-    implementation(Dependencies.firebaseAuth)
-    implementation(Dependencies.firebaseAuthUi)
-    implementation(Dependencies.firebaseAnalytics)
-    implementation(Dependencies.firebaseFirestore)
+  implementation(platform(Dependencies.firebase))
+  implementation(Dependencies.firebaseAuth)
+  implementation(Dependencies.firebaseAuthUi)
+  implementation(Dependencies.firebaseAnalytics)
+  implementation(Dependencies.firebaseFirestore)
 
-    implementation(Dependencies.navigation_fragment)
-    implementation(Dependencies.navigation_ui)
+  implementation(Dependencies.navigation_fragment)
+  implementation(Dependencies.navigation_ui)
 
-    implementation(Dependencies.hilt_android)
-    implementation(Dependencies.hilt_lifecycle_viewmodel)
-    kapt(AnnotationProcessing.hilt_android_compiler)
-    kapt(AnnotationProcessing.hilt_compiler)
+  implementation(Dependencies.hilt_android)
+  implementation(Dependencies.hilt_lifecycle_viewmodel)
+  kapt(AnnotationProcessing.hilt_android_compiler)
+  kapt(AnnotationProcessing.hilt_compiler)
 
-    implementation(Dependencies.kotlinCoroutinesPlayServices)
+  implementation(Dependencies.kotlinCoroutinesPlayServices)
 
-    implementation(Dependencies.lifecycleViewModel)
+  implementation(Dependencies.lifecycleViewModel)
 
 
 //    def nav_compose = "1.0.0-SNAPSHOT"
 //    implementation("androidx.navigation:navigation-compose:$nav_compose")
 
-    implementation(Dependencies.room)
-    implementation(Dependencies.room_runtime)
-    kapt(AnnotationProcessing.room_compiler)
+  implementation(Dependencies.room)
+  implementation(Dependencies.room_runtime)
+  kapt(AnnotationProcessing.room_compiler)
 
 
-    implementation(Dependencies.atlassian_commonMark)
+  implementation(Dependencies.atlassian_commonMark)
 
-    implementation(Dependencies.material)
+  implementation(Dependencies.material)
 
-    implementation(kotlin("stdlib-jdk8"))
+  implementation(kotlin("stdlib-jdk8"))
 
 
-    // add scripting to show the result to the user
+  // add scripting to show the result to the user
 //    def kotlin_version = kotlinVersion
 //    implementation("org.jetbrains.kotlin:kotlin-script-util:$kotlin_version")
 //    implementation("org.jetbrains.kotlin:kotlin-script-runtime:$kotlin_version")
 
 
-    // SUBPROJECTS
+  // SUBPROJECTS
 
-    implementation(project(":grammar"))
+  implementation(project(":grammar"))
 
-    // TEST
+  // TEST
 
-    testImplementation(TestDependencies.androidx_test_ext)
-    testImplementation(TestDependencies.assertJ)
-    testImplementation(TestDependencies.assertK)
-    testImplementation(TestDependencies.hiltAndroidTesting)
-    testImplementation(TestDependencies.junit4_junit)
-    testImplementation(TestDependencies.junit5_api)
-    testImplementation(TestDependencies.junit5_params)
-    testImplementation(TestDependencies.kotlinCoroutinesTest)
-    testImplementation(TestDependencies.mockk)
-    testImplementation(TestDependencies.truth)
-    testRuntimeOnly(TestDependencies.junit5_engine)
+  testImplementation(TestDependencies.androidx_test_ext)
+  testImplementation(TestDependencies.assertJ)
+  testImplementation(TestDependencies.assertK)
+  testImplementation(TestDependencies.hiltAndroidTesting)
+  testImplementation(TestDependencies.junit4_junit)
+  testImplementation(TestDependencies.junit5_api)
+  testImplementation(TestDependencies.junit5_params)
+  testImplementation(TestDependencies.kotlinCoroutinesTest)
+  testImplementation(TestDependencies.mockk)
+  testImplementation(TestDependencies.truth)
+  testRuntimeOnly(TestDependencies.junit5_engine)
 
-    androidTestImplementation(TestDependencies.androidx_test_ext)
-    androidTestImplementation(TestDependencies.assertJ)
-    androidTestImplementation(TestDependencies.assertK)
-    androidTestImplementation(TestDependencies.espresso_core)
-    androidTestImplementation(TestDependencies.espresso_contrib)
-    androidTestImplementation(TestDependencies.hiltAndroidTesting)
-    androidTestImplementation(TestDependencies.junit5_api)
-    androidTestImplementation(TestDependencies.kotlinCoroutinesTest)
-    androidTestImplementation(TestDependencies.mockkAndroid)
-    androidTestImplementation(TestDependencies.truth)
+  androidTestImplementation(TestDependencies.androidx_test_ext)
+  androidTestImplementation(TestDependencies.assertJ)
+  androidTestImplementation(TestDependencies.assertK)
+  androidTestImplementation(TestDependencies.espresso_core)
+  androidTestImplementation(TestDependencies.espresso_contrib)
+  androidTestImplementation(TestDependencies.hiltAndroidTesting)
+  androidTestImplementation(TestDependencies.junit5_api)
+  androidTestImplementation(TestDependencies.kotlinCoroutinesTest)
+  androidTestImplementation(TestDependencies.mockkAndroid)
+  androidTestImplementation(TestDependencies.truth)
 
-    kaptTest(AnnotationProcessing.hilt_compiler)
-    kaptAndroidTest(AnnotationProcessing.hilt_android_compiler)
+  kaptTest(AnnotationProcessing.hilt_compiler)
+  kaptAndroidTest(AnnotationProcessing.hilt_android_compiler)
 }
